786ee298da419a8c6dfa4f626cad5480fa018cce
Lakshmeesh <lakshmeesh625@gmail.com>
2026-06-29 11:45:39 +0530

    initial commit for typeconversions

---
+++ typeconversions.js
@@ -0,0 +1,20 @@
+let num1 = "20"
+let num2 = 10;
+let num3 = true;
+
+console.log(Number(num1)); // converts string to number
+console.log(Boolean(num1)); // converts string to boolean
+console.log(String(num2)); // converts number to string
+console.log(String(num3)); // converts boolean to string
+console.log(Boolean(num2)); // converts number to boolean
+console.log(Number(num3)); // converts boolean to number
+
+
+
+
+
+
+
+
+
+
