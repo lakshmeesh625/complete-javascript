22ea0ebc6f3baadf018788a957f911131aa0274b
Lakshmeesh <lakshmeesh625@gmail.com>
2026-06-29 10:48:11 +0530

    inital commit of arithmetic

---
+++ arithmetics.js
@@ -0,0 +1,35 @@
+let friends = 100;
+friends = friends + 5;
+console.log(`The total number of friends is ${friends}.`);
+friends = friends - 3;
+console.log(`After losing some friends, the total number of friends is ${friends}.`);
+friends = friends * 2;
+console.log(`After making new friends, the total number of friends is ${friends}.`);
+friends = friends / 4;
+console.log(`After sharing friends, the total number of friends is ${friends}.`);
+friends = friends ** 2
+console.log(`After exponenting friends , the total number of friends is ${friends}.`);
+friends = friends % 3;
+console.log(`After taking the remainder of friends, the total number of friends is ${friends}.`);
+
+
+// other method
+let bestFriends = 50 
+bestFriends += 10; // bestFriends = bestFriends + 10
+console.log(`The total number of best friends is ${bestFriends}.`);
+bestFriends -= 5;   // bestFriends = bestFriends - 5            
+console.log(`After losing some best friends, the total number of best friends is ${bestFriends}.`);
+bestFriends *= 2;   // bestFriends = bestFriends * 2
+console.log(`After making new best friends, the total number of best friends is ${bestFriends}.`);
+bestFriends /= 4;
+console.log(`After sharing best friends, the total number of best friends is ${bestFriends}.`);
+bestFriends **= 2;
+console.log(`After exponenting best friends , the total number of best friends is ${bestFriends}.`);
+bestFriends %= 3;
+console.log(`After taking the remainder of best friends, the total number of best friends is ${bestFriends}.`);
+
+
+bestFriends++;
+console.log(`After incrementing best friends, the total number of best friends is ${bestFriends}.`);
+bestFriends--;
+console.log(`After decrementing best friends, the total number of best friends is ${bestFriends}.`); 
