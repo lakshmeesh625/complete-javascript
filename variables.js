c07ff7579822cf7407014a87c9f8eb412800c7d1
Lakshmeesh <lakshmeesh625@gmail.com>
2026-06-29 09:26:16 +0530

    initial commit of variables

---
+++ variables.js
@@ -0,0 +1,9 @@
+let name = "virat"; //string
+let age = 21; // number
+let isIndian = true; // boolean
+let hobbies; // undefined
+let score = null; // null
+
+console.log(`The user\'s name is ${name} and they are ${age} years old. Are they Indian? ${isIndian}. Their hobbies are ${hobbies} and their score is ${score}.`);
+
+
